a603926361ec8b1c5406529e0542fefdfb0c150c
Ridho Febriansa <21301894+ridhof@users.noreply.github.com>
2023-10-25 10:28:24 +0700

    feat: move index into docs

--- src/pages/docs.tsx
+++ src/pages/docs.tsx
@@ -1,6 +1,9 @@
+import { BookMarked } from 'lucide-react';
 import * as React from 'react';
 
 import Layout from '@/components/layout/Layout';
+import ArrowLink from '@/components/links/ArrowLink';
+import PrimaryLink from '@/components/links/PrimaryLink';
 import Seo from '@/components/Seo';
 
 export default function HomePage() {
@@ -12,10 +15,14 @@ export default function HomePage() {
       <main>
         <section className='bg-white'>
           <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
-            <h1 className='mt-4'>Web Commerce</h1>
+            <BookMarked size={48} />
+            <h1 className='mt-4'>Revision-Style Mentorship</h1>
             <p className='mt-2 text-gray-500'>
-              Buy your favorite items anytime and anywhere with Web Commerce
+              A template for revision-style mentorship by Theodorus Clarence
             </p>
+            <ArrowLink as={PrimaryLink} className='mt-2' href='/api-tester'>
+              /api-tester
+            </ArrowLink>
           </div>
         </section>
       </main>
